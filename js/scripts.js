8309d5d043ce11f0d3ec235c81e720c08c949a6c
laisriquieri <laisriquieri@gmail.com>
2026-03-11 17:01:00 -0300

    Update

--- js/scripts.js
+++ js/scripts.js
@@ -1,63 +1,63 @@
-﻿
+﻿var WHATSAPP_NUMBER = "5516991778100";
+var WHATSAPP_BASE_URL = "https://wa.me/" + WHATSAPP_NUMBER;
 
-var WHATSAPP_NUMBER = '5516991778100';
-var WHATSAPP_BASE_URL = 'https://wa.me/' + WHATSAPP_NUMBER;
+(function ($) {
+	"use strict";
 
-(function($) {
-    "use strict"; 
-	
 	/* Preloader */
-	$(window).on('load', function() {
+	$(window).on("load", function () {
 		var preloaderFadeOutTime = 500;
 		function hidePreloader() {
-			var preloader = $('.spinner-wrapper');
-			setTimeout(function() {
+			var preloader = $(".spinner-wrapper");
+			setTimeout(function () {
 				preloader.fadeOut(preloaderFadeOutTime);
 			}, 500);
 		}
 		hidePreloader();
 	});
 
-	
 	/* Navbar Scripts */
 	// jQuery to collapse the navbar on scroll
-    $(window).on('scroll load', function() {
+	$(window).on("scroll load", function () {
 		if ($(".navbar").offset().top > 20) {
 			$(".fixed-top").addClass("top-nav-collapse");
 		} else {
 			$(".fixed-top").removeClass("top-nav-collapse");
 		}
-    });
+	});
 
 	// jQuery for page scrolling feature - requires jQuery Easing plugin
-	$(function() {
-		$(document).on('click', 'a.page-scroll', function(event) {
+	$(function () {
+		$(document).on("click", "a.page-scroll", function (event) {
 			var $anchor = $(this);
-            var targetHref = $anchor.attr('href');
-            if (!targetHref || targetHref.charAt(0) !== '#') {
-                return;
-            }
+			var targetHref = $anchor.attr("href");
+			if (!targetHref || targetHref.charAt(0) !== "#") {
+				return;
+			}
 
-            var $target = $(targetHref);
-            if ($target.length === 0) {
-                return;
-            }
+			var $target = $(targetHref);
+			if ($target.length === 0) {
+				return;
+			}
 
-			$('html, body').stop().animate({
-				scrollTop: $target.offset().top
-			}, 600, 'easeInOutExpo');
+			$("html, body").stop().animate(
+				{
+					scrollTop: $target.offset().top,
+				},
+				600,
+				"easeInOutExpo"
+			);
 			event.preventDefault();
 		});
 	});
 
-    // closes the responsive menu on menu item click
-    $(".navbar-nav li a").on("click", function(event) {
-    if (!$(this).parent().hasClass('dropdown'))
-        $(".navbar-collapse").collapse('hide');
-    });
+	// closes the responsive menu on menu item click
+	$(".navbar-nav li a").on("click", function (event) {
+		if (!$(this).parent().hasClass("dropdown"))
+			$(".navbar-collapse").collapse("hide");
+	});
 
-
-    /* Rotating Text - Morphtext */
+	/* Rotating Text - Morphtext */
 	$("#js-rotating").Morphext({
 		// The [in] animation type. Refer to Animate.css for a list of available animations.
 		animation: "fadeIn",
@@ -67,144 +67,161 @@ var WHATSAPP_BASE_URL = 'https://wa.me/' + WHATSAPP_NUMBER;
 		speed: 2000,
 		complete: function () {
 			// Called after the entrance animation is executed.
-		}
-    });
-    
-    /* Lightbox - Magnific Popup */
-	$('.popup-with-move-anim').magnificPopup({
-		type: 'inline',
-		fixedContentPos: false, /* keep it false to avoid html tag shift with margin-right: 17px */
+		},
+	});
+
+	/* Lightbox - Magnific Popup */
+	$(".popup-with-move-anim").magnificPopup({
+		type: "inline",
+		fixedContentPos: false /* keep it false to avoid html tag shift with margin-right: 17px */,
 		fixedBgPos: true,
-		overflowY: 'auto',
+		overflowY: "auto",
 		closeBtnInside: true,
 		preloader: false,
 		midClick: true,
 		removalDelay: 300,
-		mainClass: 'my-mfp-slide-bottom'
-    });
-    
-
-    /* Filter - Isotope */
-    $(window).on('load', function() {
-        var $grid = $('.grid').isotope({
-            // options
-            itemSelector: '.element-item',
-            layoutMode: 'fitRows'
-        });
-        
-        // filter items on button click
-        $('.filters-button-group').on( 'click', 'a', function() {
-            var filterValue = $(this).attr('data-filter');
-            $grid.isotope({ filter: filterValue });
-        });
-        
-        // change is-checked class on buttons
-        $('.button-group').each( function( i, buttonGroup ) {
-            var $buttonGroup = $( buttonGroup );
-            $buttonGroup.on( 'click', 'a', function() {
-                $buttonGroup.find('.is-checked').removeClass('is-checked');
-                $( this ).addClass('is-checked');
-            });	
-        });
-    });
-    
-
-    /* Counter - CountTo */
-	var a = 0;
-	$(window).scroll(function() {
-		if ($('#counter').length) { // checking if CountTo section exists in the page, if not it will not run the script and avoid errors	
-			var oTop = $('#counter').offset().top - window.innerHeight;
-			if (a == 0 && $(window).scrollTop() > oTop) {
-			$('.counter-value').each(function() {
-				var $this = $(this),
-				countTo = $this.attr('data-count');
-				$({
-				countNum: $this.text()
-				}).animate({
-					countNum: countTo
-				},
-				{
-					duration: 2000,
-					easing: 'swing',
-					step: function() {
-					$this.text(Math.floor(this.countNum));
-					},
-					complete: function() {
-					$this.text(this.countNum);
-					//alert('finished');
-					}
-				});
-			});
-			a = 1;
-			}
-		}
-    });
-
-
-    /* Move Form Fields Label When User Types */
-    // for input and textarea fields
-    $("input, textarea").keyup(function(){
-		if ($(this).val() != '') {
-			$(this).addClass('notEmpty');
-		} else {
-			$(this).removeClass('notEmpty');
-		}
-    });
-
-    /* Keep WhatsApp links consistent from a single source of truth */
-    function buildWhatsAppUrl(message) {
-        if (!message) {
-            return WHATSAPP_BASE_URL;
-        }
-        return WHATSAPP_BASE_URL + '?text=' + encodeURIComponent(message);
-    }
-
-    $('[data-wa-link]').each(function() {
-        var message = $(this).attr('data-wa-message') || '';
-        $(this).attr('href', buildWhatsAppUrl(message));
-    });
-
-    $('#callMeForm').on('submit', function(event) {
-        event.preventDefault();
-        Enviar();
-    });
-
-
-    /* Back To Top Button */
-    // create the back to top button
-    $('body').prepend('<a href="body" class="back-to-top page-scroll">Back to Top</a>');
-    var amountScrolled = 700;
-    $(window).scroll(function() {
-        if ($(window).scrollTop() > amountScrolled) {
-            $('a.back-to-top').fadeIn('500');
-        } else {
-            $('a.back-to-top').fadeOut('500');
-        }
-    });
-
-
-	/* Removes Long Focus On Buttons */
-	$(".button, a, button").mouseup(function() {
-		$(this).blur();
+		mainClass: "my-mfp-slide-bottom",
 	});
 
+	/* Filter - Isotope */
+	$(window).on("load", function () {
+		var $grid = $(".grid").isotope({
+			// options
+			itemSelector: ".element-item",
+			layoutMode: "fitRows",
+		});
+
+		// filter items on button click
+		$(".filters-button-group").on("click", "a", function () {
+			var filterValue = $(this).attr("data-filter");
+			$grid.isotope({ filter: filterValue });
+		});
+
+		// change is-checked class on buttons
+		$(".button-group").each(function (i, buttonGroup) {
+			var $buttonGroup = $(buttonGroup);
+			$buttonGroup.on("click", "a", function () {
+				$buttonGroup.find(".is-checked").removeClass("is-checked");
+				$(this).addClass("is-checked");
+			});
+		});
+	});
+
+	/* Counter - CountTo */
+	var a = 0;
+	$(window).scroll(function () {
+		if ($("#counter").length) {
+			// checking if CountTo section exists in the page, if not it will not run the script and avoid errors
+			var oTop = $("#counter").offset().top - window.innerHeight;
+			if (a == 0 && $(window).scrollTop() > oTop) {
+				$(".counter-value").each(function () {
+					var $this = $(this),
+						countTo = $this.attr("data-count");
+					$({
+						countNum: $this.text(),
+					}).animate(
+						{
+							countNum: countTo,
+						},
+						{
+							duration: 2000,
+							easing: "swing",
+							step: function () {
+								$this.text(Math.floor(this.countNum));
+							},
+							complete: function () {
+								$this.text(this.countNum);
+								//alert('finished');
+							},
+						}
+					);
+				});
+				a = 1;
+			}
+		}
+	});
+
+	/* Move Form Fields Label When User Types */
+	// for input and textarea fields
+	$("input, textarea").keyup(function () {
+		if ($(this).val() != "") {
+			$(this).addClass("notEmpty");
+		} else {
+			$(this).removeClass("notEmpty");
+		}
+	});
+
+	/* Keep WhatsApp links consistent from a single source of truth */
+	function buildWhatsAppUrl(message) {
+		if (!message) {
+			return WHATSAPP_BASE_URL;
+		}
+		return WHATSAPP_BASE_URL + "?text=" + encodeURIComponent(message);
+	}
+
+	$("[data-wa-link]").each(function () {
+		var message = $(this).attr("data-wa-message") || "";
+		$(this).attr("href", buildWhatsAppUrl(message));
+	});
+
+	$("#callMeForm").on("submit", function (event) {
+		event.preventDefault();
+		Enviar();
+	});
+
+	/* Back To Top Button */
+	// create the back to top button
+	$("body").prepend(
+		'<a href="#header" class="back-to-top page-scroll">Back to Top</a>'
+	);
+	var amountScrolled = 700;
+	$(window).scroll(function () {
+		if ($(window).scrollTop() > amountScrolled) {
+			$("a.back-to-top").fadeIn("500");
+		} else {
+			$("a.back-to-top").fadeOut("500");
+		}
+	});
+
+	/* Removes Long Focus On Buttons */
+	$(".button, a, button").mouseup(function () {
+		$(this).blur();
+	});
 })(jQuery);
 
-function Enviar(){
-    var nome = document.getElementById("nomeid").value.trim();
-    var telefone = document.getElementById("telefoneid").value.trim();
-    var mensagem = document.getElementById("mensagemid").value.trim();
-    var texto = '';
+function Enviar() {
+	var nome = document.getElementById("nomeid").value.trim();
+	var telefone = document.getElementById("telefoneid").value.trim();
+	var mensagem = document.getElementById("mensagemid").value.trim();
+	var texto = "";
 
-    if (nome !== "" && telefone !== "" && mensagem !== "") {
-        texto = "Ola, meu nome e " + nome + ", meu telefone para contato e " + telefone + " e desejo " + mensagem;
-    } else if (nome !== "" && telefone !== "") {
-        texto = "Ola, meu nome e " + nome + ", meu telefone para contato e " + telefone + " e desejo saber mais sobre a Lemes Informatica";
-    } else if (nome !== "") {
-        texto = "Ola, meu nome e " + nome + " e desejo saber mais sobre a Lemes Informatica";
-    } else {
-        return;
-    }
+	if (nome !== "" && telefone !== "" && mensagem !== "") {
+		texto =
+			"Ola, meu nome e " +
+			nome +
+			", meu telefone para contato e " +
+			telefone +
+			" e desejo " +
+			mensagem;
+	} else if (nome !== "" && telefone !== "") {
+		texto =
+			"Ola, meu nome e " +
+			nome +
+			", meu telefone para contato e " +
+			telefone +
+			" e desejo saber mais sobre a Lemes Informatica";
+	} else if (nome !== "") {
+		texto =
+			"Ola, meu nome e " +
+			nome +
+			" e desejo saber mais sobre a Lemes Informatica";
+	} else {
+		return;
+	}
 
-    window.open(WHATSAPP_BASE_URL + "?text=" + encodeURIComponent(texto), "_blank", "noopener,noreferrer");
+	window.open(
+		WHATSAPP_BASE_URL + "?text=" + encodeURIComponent(texto),
+		"_blank",
+		"noopener,noreferrer"
+	);
 }
